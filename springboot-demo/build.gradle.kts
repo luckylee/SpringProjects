ba948ca1b171b7b93145a4ed29a26da1602ab9e7
Wegin Lee <weginlee@gmail.com>
2022-03-16 11:10:04 +0800

    Add support to connect H2 database
    - In memory to create database testdb and connect it.

--- springboot-demo/build.gradle.kts
+++ springboot-demo/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	runtimeOnly("com.h2database:h2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
