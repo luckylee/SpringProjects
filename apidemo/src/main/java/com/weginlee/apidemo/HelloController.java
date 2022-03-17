aa22a621f03ec3741d9737b1a924efb52b09b83a
Wegin Lee <weginlee@gmail.com>
2022-03-17 13:18:29 +0800

    Junit Test for spring boot.
    -After Sprring boot 2.3, the default unit test is JUnit5
    - Make build.gradle with Junit 5 support
    - Make Test file with Junit5 import and annotation.

--- apidemo/src/main/java/com/weginlee/apidemo/HelloController.java
+++ apidemo/src/main/java/com/weginlee/apidemo/HelloController.java
@@ -1,14 +1,18 @@
 package com.weginlee.apidemo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String index() {
-        return "Welcome to the world of SpringBoot!";
+    //public String hello(@RequestParam(value = "name", defaultValue = "Wegin") String name) {
+    public String hello() {
+        // Use the http://localhost:8080/hello?name=Sophia to try
+        // return String.format("Welcome to the world of SpringBoot, %s !", name);
+        return String.format("Hello World");
     }
 
 }
--- apidemo/src/test/java/com/weginlee/apidemo/ApidemoApplicationTests.java
+++ apidemo/src/test/java/com/weginlee/apidemo/ApidemoApplicationTests.java
@@ -1,13 +1,44 @@
 package com.weginlee.apidemo;
 
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 class ApidemoApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private MockMvc mvc;
+
+	@BeforeEach
+	void setUp() {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+
 	}
 
+	@Test
+	void contextLoads() throws Exception {
+		//Assume.assumeNotNull(mvc);
+
+		RequestBuilder request;
+
+		request = get( "/hello" );
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World")));
+		//.andExpect(content().string(equalTo("Welcome to the world of SpringBoot, Wegin")));
+
+	}
 }
