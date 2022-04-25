5379e9dc8238223092935192198619ec532971cd
Wegin Lee <weginlee@gmail.com>
2022-04-25 11:40:02 +0800

    Refine chapter 2 basic functions.
    - Refine background image, log info in controllers and errors check
    - TODO: Add errors display in View
    - TODO: Add tests for controllers

--- taco-cloud/src/main/java/com/example/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/com/example/tacos/web/OrderController.java
@@ -1,17 +1,16 @@
 package com.example.tacos.web;
 
+import com.example.tacos.Taco;
 import com.example.tacos.TacoOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -20,7 +19,13 @@ import javax.validation.Valid;
 public class OrderController {
 
     @GetMapping("/current")
-    public String orderForm() {
+    public String orderForm(@ModelAttribute TacoOrder tacoOrder) {
+        // TODO() Get Taco object and log the ingredients
+        List<Taco> tacos = tacoOrder.getTacos();
+        for (Taco taco : tacos) {
+            log.info("Taco name: {} ", taco.getName());
+        }
+
         return "orderForm";
     }
 
