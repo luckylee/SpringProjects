21b21566934088a75551f70aca58fb909fc48ca3
Wegin Lee <weginlee@gmail.com>
2022-04-28 15:57:02 +0800

    Fix HomeControllerTest fail
    - Figure out why it failed and how this patch fix it.
    - All initialized controllers, DesignTacoController and OrderController need to pass the related repositories to these constructors.
    - During the test, it also needs to mock those repositories and the setup the application in the test
    - TODO: Develop more unit tests for each controller.

--- taco-cloud/src/main/java/com/example/tacos/TacoCloudApplication.java
+++ taco-cloud/src/main/java/com/example/tacos/TacoCloudApplication.java
@@ -6,7 +6,10 @@ import com.example.tacos.domain.Ingredient.Type;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -33,5 +36,24 @@ public class TacoCloudApplication {
 			}
 		};
 	}
+
+
+	@Bean
+	// List all the beans/components in the initialization.
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
+
 
 }
--- taco-cloud/src/test/java/com/example/tacos/HomeControllerTest.java
+++ taco-cloud/src/test/java/com/example/tacos/HomeControllerTest.java
@@ -6,9 +6,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import com.example.tacos.data.IngredientRepository;
+import com.example.tacos.data.OrderRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
@@ -17,13 +20,20 @@ public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private IngredientRepository ingredientRepository;
+
+    @MockBean
+    private OrderRepository orderRepository;
+
+
     @Test
     public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
                 .andExpect(content().string(
-                        containsString("Welcome to...")));
+                        containsString("Welcome to the Magic Taco-Could store")));
     }
 
 }
